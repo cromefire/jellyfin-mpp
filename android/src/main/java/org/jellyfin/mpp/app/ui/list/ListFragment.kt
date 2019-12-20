28c3a80daed15fce660fdfbcfc4d24df55a20277
cromefire <tim.langhorst@outlook.de>
2019-12-20 17:42:21 +0100

    Fixed crashlytics

---
+++ android/src/main/java/org/jellyfin/mpp/app/ui/list/ListFragment.kt
@@ -0,0 +1,30 @@
+package org.jellyfin.mpp.app.ui.list
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
+import org.jellyfin.mpp.app.R
+
+class ListFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = ListFragment()
+    }
+
+    private lateinit var viewModel: ListViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.list_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(ListViewModel::class.java)
+    }
+}
---
+++ android/src/main/java/org/jellyfin/mpp/app/ui/list/ListViewModel.kt
@@ -0,0 +1,7 @@
+package org.jellyfin.mpp.app.ui.list
+
+import androidx.lifecycle.ViewModel
+
+class ListViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
